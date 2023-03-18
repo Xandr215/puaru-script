f0ba25023ed5dc77f0e4bf29b48e61cdc84acb4a
Xandr215 <xandr215-github@outlook.com>
2023-03-18 13:15:14 -0500

    Updated puaru.sh
    put autoremove back where it was

--- puaru.sh
+++ puaru.sh
@@ -1,6 +1,6 @@
 echo Starting PUARU script...
-sudo apt-get autoremove || error 'Could not APT-Get Autoremove!'
 sudo apt-get update || error 'Could not APT-Get Update!'
 sudo apt-get upgrade || error 'Could not APT-Get Upgrade!'
+sudo apt-get autoremove || error 'Could not APT-Get Autoremove!'
 sudo apt-get autoclean || error 'Could not APT-Get Autoclean!'
 echo PUARU script finished! Now exiting... || exit 1
